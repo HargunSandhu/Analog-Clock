2a6f07e4e98a5e7a79a71437f105a8874edd0570
Hargun Sandhu <98478364+HargunSandhu@users.noreply.github.com>
2022-05-12 21:10:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
-f
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
